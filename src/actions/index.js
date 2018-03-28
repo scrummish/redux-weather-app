a723e07513fe2a8b6ca32a79c210cc55328e99ca
sergio <scrummish@gmail.com>
2018-03-28 03:11:41 -0500

    Installed axios in order to make ajax requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
 const API_KEY = '65350b06e7cc6faab2216e0be264fa3d';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+// Action Creator: 
+export function fetchWeather(city){
+const url = `${ROOT_URL}&q=${city},us`;
+	// Returns Action: 
+	return {
+		type: FETCH_WEATHER
+	};
+}
